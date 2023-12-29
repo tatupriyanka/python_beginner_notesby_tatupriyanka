e38e5da0cb27387948e26a9d4c92289c1d4fb41c
tatupriyanka <155098140+tatupriyanka@users.noreply.github.com>
2023-12-29 14:10:21 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,25 @@
+#A Key Point to know about Python
+# It is a case sensitive language
+1)
+print("hello priyanka")
+#Variables:-
+#Basic Types in Python - numbers(integers, floating), boolean, strings
+2)
+name = "shradha"
+age = 22
+print(name)
+print(age)
+3)
+name = "priyanka"
+age = 21
+name = "sakshi"
+print(name)
+print(age)
+4)
+first_name = "priyanka"
+last_name = "tatu"
+age = 21
+is_adult = True
+print(first_name + " " + last_name)
+print(age)
+print(is_adult)
